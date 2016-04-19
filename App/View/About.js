c82b35d00ca8ef0984cb0e4391b24a0a9a797439
jaywcjlove <398188662@qq.com>
2016-04-19 23:31:45 +0800

    解决警告没有key

--- App/View/About.js
+++ App/View/About.js
@@ -14,22 +14,27 @@ import ListItemWrapper from './ListItemWrapper';
 
 //cat 数据
 var MockData = [{
+  id: 1,
   img : 'http://gtms02.alicdn.com/tps/i2/TB1hbkyHpXXXXboXXXXcy0wIpXX-70-70.png',
   text : '手机圈儿',
   link : 'http://3c.m.tmall.com'                      
 },{
+  id: 2,
   img : 'http://gtms01.alicdn.com/tps/i1/TB13zsxHpXXXXX8XpXXcy0wIpXX-70-70.png',
   text : '发现好玩',
   link : 'http://3c.m.tmall.com'                      
 },{
+  id: 3,
   img : 'http://gtms01.alicdn.com/tps/i1/TB1wpUtHpXXXXb1XVXXcy0wIpXX-70-70.png',
   text : '我爱我家',
   link : 'http://3c.m.tmall.com'                      
 },{
+  id: 4,
   img : 'http://gtms03.alicdn.com/tps/i3/TB14NwyHpXXXXaUXXXXcy0wIpXX-70-70.png',
   text : '生活圈儿',
   link : 'http://3c.m.tmall.com'                      
 },{
+  id: 5,
   img : 'http://gtms04.alicdn.com/tps/i4/TB1ODktHpXXXXXZXVXXcy0wIpXX-70-70.png',
   text : '试用中心',
   link : 'http://3c.m.tmall.com'                      
@@ -51,8 +56,9 @@ const styles = StyleSheet.create({
 class About extends React.Component{
     renderItems(data){
         return data.map(function(result){
+          console.log("result:",result.id);
           return (
-            <ListItemWrapper data={result}/>
+            <ListItemWrapper key={result.id} data={result}/>
           )
         })
     }
